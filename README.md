d215daf87a77ca8889dbd44411ea0177e3ad7b74
Tyler <tylerrmoore@gmail.com>
2025-10-05 17:50:24 -0700

    Working on environment

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # security-controller
 Security controller using embedded rust
+
+# Dependencies
+Have to install ravedude from https://github.com/Rahix/avr-hal. `cargo install ravedude`
---
+++ default.nix
@@ -0,0 +1,35 @@
+# default.nix
+{ pkgs ? import <nixpkgs> {} }:
+
+
+pkgs.mkShell {
+
+    # Certain Rust tools won't work without this
+    # This can also be fixed by using oxalica/rust-overlay and specifying the rust-src extension
+    # See https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/3?u=samuela. for more details.
+    RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+    
+    nativeBuildInputs = with pkgs; [
+        #From nixos.wiki/wiki/Rust
+        rustc
+        cargo
+        gcc
+        rustfmt
+        clippy
+
+        #For avr programming
+        pkgsCross.avr.buildPackages.gcc
+        pkg-config
+        avrdude
+        udev
+
+        #Ravedude, a nice rust wrapper for avrdude
+        ravedude
+
+        #Cargo-generate dependencies
+        zlib
+        openssl
+    ];   
+
+}
+#lib/rustlib/src/rust/library/Cargo.lock
