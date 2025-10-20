37f2ddcef8e6d892dd5cfbfde5c4157c02b67ac0
Tyler <tylerrmoore@gmail.com>
2025-10-20 12:28:55 -0700

    Security controller pins

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,12 @@ use core::cell::RefCell;
 pub type Console = arduino_hal::hal::usart::Usart0<arduino_hal::DefaultClock>;
 pub static CONSOLE: interrupt::Mutex<RefCell<Option<Console>>> = interrupt::Mutex::new(RefCell::new(None));
 
+pub fn put_console(console: Console) {
+    interrupt::free(|cs| {
+        *CONSOLE.borrow(cs).borrow_mut() = Some(console);
+    })
+}
+
 #[macro_export]
 macro_rules! print {
     ($($t:tt)*) => {
--- src/main.rs
+++ src/main.rs
@@ -4,32 +4,33 @@
 use panic_halt as _;
 
 use avr_device::interrupt;
-use security_controller::{println, Console, CONSOLE};
-
-fn put_console(console: Console) {
-    interrupt::free(|cs| {
-        *CONSOLE.borrow(cs).borrow_mut() = Some(console);
-    })
-}
+use security_controller::{println, put_console, Console, CONSOLE};
 
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
+    
     let pins = arduino_hal::pins!(dp);
+
     let serial = arduino_hal::default_serial!(dp, pins, 57600);
+    
     put_console(serial);
 
-    /*
-     * For examples (and inspiration), head to
-     *
-     *     https://github.com/Rahix/avr-hal/tree/main/examples
-     *
-     * NOTE: Not all examples were ported to all boards!  There is a good chance though, that code
-     * for a different board can be adapted for yours.  The Arduino Uno currently has the most
-     * examples available.
-     */
+    //https://github.com/Rahix/avr-hal/tree/main/examples
 
+    //Just for testing
     let mut led = pins.d13.into_output();
+
+    let sensor1 = pins.d4.into_pull_up_input();
+    let sensor2 = pins.d5.into_pull_up_input();
+    let sensor3 = pins.d6.into_pull_up_input();
+    let sensor4 = pins.d7.into_pull_up_input();
+    let sensor5 = pins.d8.into_pull_up_input();
+    let sensor6 = pins.d9.into_pull_up_input();
+
+    let motion_sensor_1 = pins.d2.into_pull_up_input();
+
+    let mut siren1=pins.d3.into_output();
 
     println!("Hi!");
 
